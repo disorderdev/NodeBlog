f5dfc72725015ba960d3024af9e7d862bf0ce716
disorderdev <disorderdev@gmail.com>
2012-01-29 20:17:21 +0800

    response to /

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var url = require('url');
 
 var server = http.createServer(function(req, res) {
-     res.writeHead(200, {'Content-Type':'text/plain'});
-     res.write('OK');
-     res.end();
+    var method = req.method;
+    var urls = url.parse(req.url, true);
+    var pathname = urls.pathname;
+    var queries = urls.query;
+
+    if (method == 'GET') {
+        if (pathname == '/' || pathname == '/list') {
+            fs.readFile('list.html', 'binary', function (err, data) {
+                if (err) throw err;
+                res.writeHead(200, {'Content-Type':'text/html'});
+                res.write(data, 'binary');
+                res.end();
+            });
+        } else {
+            res.writeHead(404, {'Content-Type':'text/plain'});
+            res.end();
+        }
+    }else {
+        res.writeHead(404, {'Content-Type':'text/plain'});
+        res.end();                    
+    }
 });
 
 server.listen(1337, '127.0.0.1');
