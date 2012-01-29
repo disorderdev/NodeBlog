0900d72d4e47b0804b97f65b9eebf53e725dfe1d
disorderdev <disorderdev@gmail.com>
2012-01-29 20:33:05 +0800

    create new blog

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 var url = require('url');
+var querystring = require('querystring');
 
 var server = http.createServer(function(req, res) {
     var method = req.method;
@@ -9,21 +10,40 @@ var server = http.createServer(function(req, res) {
     var pathname = urls.pathname;
     var queries = urls.query;
 
+
     if (method == 'GET') {
-        if (pathname == '/' || pathname == '/list') {
+        if (pathname == '/favicon.ico') {
+            res.writeHead(404, 'Not found');
+            res.end();
+        } else if (pathname == '/' || pathname == '/list') {
             fs.readFile('list.html', 'binary', function (err, data) {
                 if (err) throw err;
                 res.writeHead(200, {'Content-Type':'text/html'});
                 res.write(data, 'binary');
                 res.end();
             });
-        } else {
-            res.writeHead(404, {'Content-Type':'text/plain'});
-            res.end();
+        } else if (pathname == '/jquery.js') {
+            fs.readFile('jquery.js', 'binary', function (err, data) {
+                if (err) throw err;
+                res.writeHead(200, {'Content-Type':'text/html'});
+                res.write(data, 'binary');
+                res.end();
+            });
         }
-    }else {
-        res.writeHead(404, {'Content-Type':'text/plain'});
-        res.end();                    
+    }else if (method == 'POST') {
+        if (pathname = '/new') {
+            req.on('data', function(data) {
+                var buf = "" + data;
+                var b = querystring.parse(buf);
+                if (b.title) {
+                    fs.writeFile('blogs/' + b.title, b.content, function(err) {
+                        if (err) throw err;
+                    res.writeHead(200, {'Content-Type':'text/plain'});
+                    res.end();
+                    });
+                }
+            });
+        }
     }
 });
 
